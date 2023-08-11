53c4d50f294c48a54d3761bfc77cac2670750479
Bentley Hensel <bentley.hensel@civicactions.com>
2023-08-10 20:14:20 -0400

    Working homepage summary

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,9 @@
 # app/routes.py
 from app import app
+import os
 from app.views import index, metrics, trigger_error
 import requests
-from flask import render_template
+from flask import render_template, jsonify, request
 
 
 @app.route('/')
@@ -20,3 +21,14 @@ def get_metrics():
 @app.route('/debug-sentry')
 def debug_sentry():
     return trigger_error()
+
+@app.route('/domain/summary')
+def get_domain_summary():
+    domain = request.args.get('domain')
+    url = f"https://reports.openato.com/domain/summary?domain={domain}&format=json"
+    headers = {
+        "CF-Access-Client-Id": os.environ.get('CF_ACCESS_CLIENT_ID'),
+        "CF-Access-Client-Secret": os.environ.get('CF_ACCESS_CLIENT_SECRET'),
+    }
+    response = requests.get(url, headers=headers)
+    return jsonify(response.json())
--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,36 +1,40 @@
 // app/static/js/main.js
 function handleDomainClick(domain) {
-  const url = `https://reports.openato.com/domain/summary?domain=${domain}&format=json`;
+  const url = `/domain/summary?domain=${domain}`;
 
-  // Perform AJAX request (using fetch here, but could use jQuery or other method)
   fetch(url)
     .then(response => response.json())
     .then(data => {
-      // Assuming more than one item might be returned, taking the first one
-      const domainInfo = data[0];
-
-      // Function to render the domain information to the DOM
-      renderDomainInfo(domainInfo);
+      console.log(data); // Inspect the data here
+      renderDomainInfo(data); // Directly pass the data object to render
     })
     .catch(error => {
       console.error("Error fetching domain info:", error);
     });
 }
 
-function renderDomainInfo(info) {
-  // Example of displaying some of the data - adjust as needed
+
+function renderDomainInfo(infoArray) {
+  const info = infoArray[0]; // Get the first object from the array
+
+  // Display the values...
+  document.getElementById('active-urls').innerText = info.active_urls.toLocaleString();
   document.getElementById('domain-name').innerText = info.domain;
-  document.getElementById('active-urls').innerText = info.active_urls;
-  document.getElementById('url-count').innerText = info.url_count;
-  // Add more fields as required
+  document.getElementById('errored-urls').innerText = info.errored_urls.toLocaleString();
+  document.getElementById('monitored-urls').innerText = info.objective_urls.toLocaleString();
+  document.getElementById('url-count').innerText = info.url_count.toLocaleString();
 }
+
+
+
 
 $(document).ready(function() {
   $(".domain-list li a").click(function(event) {
-    event.preventDefault(); // Prevent the default link behavior
-    const domain = $(this).text(); // Get the clicked domain name
-    handleDomainClick(domain); // Call the function defined earlier to handle the click
+    event.preventDefault();
+    console.log("Domain clicked!"); // Log a message when a domain is clicked
+    const domain = $(this).text();
+    handleDomainClick(domain);
   });
-  $("#domain-info").show(); // Make sure the domain info is shown
 });
+
 
